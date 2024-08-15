4da697628ce244a0172e6c7a9a0cb0c1b2579b2b
Hazem Zaki <hazemzaki.work@gmail.com>
2024-08-15 13:22:57 +0300

    Apply Pagination

--- src/components/AllProducts.vue
+++ src/components/AllProducts.vue
@@ -1,18 +1,39 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onBeforeUpdate } from 'vue';
 import axios from 'axios';
 import ProductCard from './ProductCard.vue';
 
 const products = ref([]);
+const currentPage = ref(1);
+
+const changePage = (event) => {
+    if (event.target.textContent === "Previous" && currentPage.value !== 1) {
+        currentPage.value--;
+    } else if (event.target.textContent === "Next") {
+        currentPage.value++;
+    } else if (event.target.textContent !== "Previous") {
+        currentPage.value = parseInt(event.target.textContent);
+    }
+};
 
 onMounted(async () => {
     try {
-        const response = await axios.get('http://localhost:8080/api/products');
+        const response = await axios.get('http://localhost:8080/api/products/page/' + currentPage.value);
         products.value = response.data;
     } catch (error) {
         console.error('Error fetching products data', error);
     }
 });
+
+onBeforeUpdate(async () => {
+    try {
+        const response = await axios.get('http://localhost:8080/api/products/page/' + currentPage.value);
+        products.value = response.data;
+    } catch (error) {
+        console.error('Error fetching products data', error);
+    }
+});
+
 </script>
 
 <template>
@@ -27,11 +48,11 @@ onMounted(async () => {
 
     <nav aria-label="Page navigation example" class="d-flex justify-content-center" style="margin-top: 20px;">
         <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            <li @click="changePage($event)" class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li @click="changePage($event)" class="page-item"><a class="page-link" href="#">1</a></li>
+            <li @click="changePage($event)" class="page-item"><a class="page-link" href="#">2</a></li>
+            <li @click="changePage($event)" class="page-item"><a class="page-link" href="#">3</a></li>
+            <li @click="changePage($event)" class="page-item"><a class="page-link" href="#">Next</a></li>
         </ul>
     </nav>
 </template>
